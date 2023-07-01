10d6362bdddcaa8052b3d1e538d238cb039dbd51
Nikhil <nikhilsahu1312@gmail.com>
2023-07-01 13:20:47 +0530

    added deployed frontend link

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const server = require('http').createServer(app)
 const PORT = 5001
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://chat-hub-frontend.vercel.app/'],
         methods: ['GET', 'POST']
     },
 })
