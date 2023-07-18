19dd6854d3666fea365392e865ebe6b2418020ae
Nikhil <nikhilsahu1312@gmail.com>
2023-07-19 00:33:04 +0530

    added chat header

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,12 +4,15 @@ import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 import Contacts from '../components/Contacts'
 import { allUsersRoute } from "../utils/APIRoutes";
+import Welcome from '../components/Welcome'
+import ChatContainer from '../components/ChatContainer'
 
 const Chat = () => {
   const navigate = useNavigate()
   const [contacts, setContacts] = useState([])
-  const [currentUser, setCurrentUser] = useState(undefined)
+  const [currentUser, setCurrentUser] = useState(null)
   const [currentChat, setCurrentChat] = useState(undefined)
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
     const fetch = async () => {
@@ -17,6 +20,7 @@ const Chat = () => {
         navigate('/login')
       } else {
         setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')))
+        setIsLoaded(true) // for fully loading of currentUser
       }
     }
     fetch()
@@ -45,6 +49,13 @@ const Chat = () => {
     <Container>
       <div className="container">
         <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+        {
+          isLoaded && currentChat === undefined ? (
+            <Welcome currentUser={currentUser} />
+          ) : (
+            <ChatContainer currentChat={currentChat} />
+          )
+        }
       </div>
     </Container>
   )
