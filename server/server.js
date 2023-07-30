d2965d987f424bd53d28d6e76405d0947ec4108b
Nikhil Kumar Sahu <75385653+nikhil822@users.noreply.github.com>
2023-07-30 17:28:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messageRoute = require("./routes/messageRoute");
 const socket = require('socket.io')
-import { Server } from "socket.io";
 
 app.use(express.json());
 app.use(cors({
@@ -31,20 +30,14 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`)
 })
 
-// const io = socket(server, {
-//   cors: {
-//     origin: 'https://chat-hub-frontend.vercel.app',
-//     credentials: true,
-//   },
-//   transports: ['polling']
-// })
-const io = new Server(server, {
+const io = socket(server, {
   cors: {
     origin: 'https://chat-hub-frontend.vercel.app',
     credentials: true,
   },
-  path: "/my-custom-path/"
-});
+  transports: ["websocket"]
+})
+
 global.onlineUsers = new Map()
 
 io.on('connection', (socket) => {
